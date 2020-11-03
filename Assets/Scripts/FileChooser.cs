a5926f0eae5365471a7cb9a51919aadfceec1b68
Yuhan Wu <u1274265@utah.edu>
2020-11-02 23:08:41 -0700

    Initial commit

---
+++ Assets/Scripts/FileChooser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class FileChooser : MonoBehaviour
+{
+    public InputField FilePathTextArea;
+
+    public void ShowFilePanel()
+    {
+        string path = EditorUtility.OpenFilePanel("Choose a song", "", "mp3, wav");
+        FilePathTextArea.text = path;
+    }
+}
